d8489937e24d213273f21a641557cad9f67e673a
SunnyBarua <sunnybarua95@gmail.com>
2020-03-21 01:44:05 +0600

    brought changes

--- src/components/Shipment/Shipment.js
+++ src/components/Shipment/Shipment.js
@@ -8,37 +8,43 @@ const Shipment=()=> {
     const onSubmit = data => { console.log(data) }
     const auth=useAuth();
   
-    
-  
     return (
       
       <form className="ship-form" onSubmit={handleSubmit(onSubmit)} >
 
-        
-      
-       
-       
         <input name="name" defaultValue={auth.user.name} ref={register({ required: true })} placeholder="You Name"/>
         
-        {errors.name && <span className="error">Name is required</span>}
+        {
+        errors.name && <span className="error">Name is required</span>
+        }
         <input name="email" defaultValue={auth.user.email} ref={register({ required: true })} placeholder="Your Email" />
         
-        {errors.email && <span  className="error">Email is required</span>}
+        {
+        errors.email && <span  className="error">Email is required</span>
+        }
         <input name="AddressLine1" ref={register({ required: true })} placeholder="Address Line 1"/>
-        {errors.AddressLine1 && <span className="error">Address is required</span>}
+        {
+        errors.AddressLine1 && <span className="error">Address is required</span>
+        }
         
         <input name="AddressLine2" ref={register} placeholder="Address Line 2" />
         
         <input name="city" ref={register({ required: true })} placeholder="City"/>
-        {errors.city && <span  className="error">City is required</span>}
+        {
+        errors.city && <span  className="error">City is required</span>
+        }
 
         <input name="country" ref={register({ required: true })} placeholder="Country"/>
         
-        {errors.country && <span  className="error">Country is required</span>}
+        {
+        errors.country && <span  className="error">Country is required</span>
+        }
 
         <input name="zipcode" ref={register({ required: true })} placeholder="Zip Code" />
         
-        {errors.zipcode && <span  className="error">Zip Code is required</span>}
+        {
+        errors.zipcode && <span  className="error">Zip Code is required</span>
+        }
         
         <input type="submit" />
       </form>
